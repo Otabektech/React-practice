ee4ab26f444f536dc323756f3874e6e4a85c0510
Otabek Rakhimov <rahimovmail@gmail.com>
2022-12-04 12:26:42 -0800

    Add content to the project

--- src/index.js
+++ src/index.js
@@ -7,52 +7,44 @@ import ReactDom from "react-dom";
 // CSS
 import "./index.css";
 
-const firstBook = {
-  img: "https://m.media-amazon.com/images/I/51LuOq2b8aL.jpg",
-  title: "Let It Snow",
-  author: "Beth Moran",
-};
+const books = [
+  {
+    img: "https://m.media-amazon.com/images/I/51LuOq2b8aL.jpg",
+    title: "Let It Snow",
+    author: "Beth Moran",
+  },
 
-const secondBook = {
-  img: "https://m.media-amazon.com/images/I/51ghwawugqL._BG0,0,0,0_FMpng_AC_SY320_SX320_.jpg",
-  title: "The Christmas Lodge",
-  author: "Fiona Baker",
-};
+  {
+    img: "https://m.media-amazon.com/images/I/51ghwawugqL._BG0,0,0,0_FMpng_AC_SY320_SX320_.jpg",
+    title: "The Christmas Lodge",
+    author: "Fiona Baker",
+  },
+
+  {
+    img: "https://m.media-amazon.com/images/I/51gNNGZBXqL._BG0,0,0,0_FMpng_AC_SY320_SX320_.jpg",
+    title: "Second Chance Christmas",
+    author: "Fiona Baker",
+  },
+];
 
 function BookList() {
   return (
     <section className='booklist'>
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, iste
-          nam, molestias sunt consectetur quaerat minima ipsa labore tempora
-          nobis eveniet eius. Exercitationem consectetur at suscipit ipsum
-          corporis necessitatibus aliquam!
-        </p>
-      </Book>
-
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
+      {books.map((book) => {
+        return <Book book={book}></Book>;
+      })}
     </section>
   );
 }
 
 const Book = (props) => {
-  const { img, title, author, children } = props;
+  const { img, title, author } = props.book;
 
   return (
     <article className='book'>
       <img src={img} alt='' />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {children}
     </article>
   );
 };
